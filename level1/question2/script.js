a58db5bc7b837aa68048312ee5a9dcfca4fc812c
Unknown <wavetraxx@hotmail.com>
2018-10-27 13:57:35 +0200

    question 2

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -6,22 +6,26 @@ fetch('https://jsonplaceholder.typicode.com/todos')
     .catch(err => console.log(err));
 
 function createCard(result){
-    console.log(result.length);
-
     for(var i = 0; i < result.length; i++) {
-
         var divCont = document.createElement('div');
         divCont.className = 'card-container';
         var div = document.createElement('div');
         div.className = 'todo-card';
+
         div.innerHTML += "<div class='userID cardInfo'>User ID: " + result[i].userId + "</div>";
+        div.innerHTML += "<div class='cardTitle cardInfo'>Task: " + result[i].title + "</div>";
         div.innerHTML += "<div class='cardID cardInfo'>Card ID:   " + result[i].id + "</div>";
-        div.innerHTML += "<div class='cardTitle cardInfo'>Title: " + result[i].title + "</div>";
-        div.innerHTML += "<div class='cardStatus cardInfo'>Completed:   " + result[i].completed + "</div>";
+
+        if (result[i].completed === true) {
+            divCont.style.backgroundColor = '#AAFFC2';
+            div.innerHTML += "<div class='cardStatus cardInfo'>Completed: &#10004;</div>";
+        } else {
+            divCont.style.backgroundColor = '#B25B5B';
+            div.innerHTML += "<div class='cardStatus cardInfo'>Completed: &#10006;</div>";
+        }
+
         divCont.appendChild(div);
         document.getElementsByTagName('body')[0].appendChild(divCont);
-
-
     }
 }
 
